79bdbe53d198fedb76e22d83f148dd3a9a486ee1
Joel <joelinyang2003@gmail.com>
2022-07-01 12:28:29 -0700

    fi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * with proper grammar
+ * return 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
