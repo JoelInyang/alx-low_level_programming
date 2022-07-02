2913e448da749ce86e1cdf66fae4100fcddaf45e
Joel <joelinyang2003@gmail.com>
2022-07-02 16:51:30 -0700

    fir

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	char charType;
 	int intType;
-	double doubleType;
+	double int doubleIntType;
 	float floatType;
 
 
 	printf("Size of a char :%s byte(s)\n", charType);
 	printf("Size of an int :%d byte(s)\n", intType);
-	printf("Size of a double :%d byte(s)\n", doubleType);
-	printf("Size of a floatType :%d byte(s)\n", floatType);
+	printf("Size of a double int :%d byte(s)\n", doubleIntType);
+	printf("Size of a float :%d byte(s)\n", floatType);
 
 	return (0);
 }
