facf5f0af8c5831bb5c06541c4b1e1557a95b7a0
Joel <joelinyang2003@gmail.com>
2022-07-02 16:44:18 -0700

    fir

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	char c;
+	char charType;
+	int intType;
+	double doubleType;
+	float floatType;
 
-	printf("The size of various types on the computer it compiler and run on\n", c);
+
+	printf("Size of a char :%s byte(s)\n", charType);
+	printf("Size of an int :%d byte(s)\n", intType);
+	printf("Size of a double :%d byte(s)\n", doubleType);
+	printf("Size of a floatType :%d byte(s)\n", floatType);
 
 	return (0);
 }
