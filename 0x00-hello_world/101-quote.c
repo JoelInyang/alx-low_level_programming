64806a73fbe681df51dfe8045d2468d395eac1a3
Joel <joelinyang2003@gmail.com>
2022-07-03 04:00:05 -0700

    first

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: 1
+ */
+int main(void)
+i
+{
+	char var[] = "and that piece of art is useful\" - Dora, 2015-10-19\n";
+
+	write(var, sizeof(var) - 1);
+
+	return (1);
+}
