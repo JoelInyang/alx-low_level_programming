c0312697abdab050b10ab1cb0cc61fc1855f622b
Joel <joelinyang2003@gmail.com>
2022-07-02 17:36:16 -0700

    fir

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	float floatType;
 
 
-	printf("Size of a char :%s byte(s)\n", charType);
-	printf("Size of an int :%d byte(s)\n", intType);
-	printf("Size of a long int :%d byte(s)\n" longIntType);
-	printf("Size of a long long int :%d byte(s)\n" longLongType);
-	printf("Size of a float :%d byte(s)\n", floatType);
+	printf("Size of a char :%ld byte(s)\n", charType);
+	printf("Size of an int :%ld byte(s)\n", intType);
+	printf("Size of a long int :%ld byte(s)\n" longIntType);
+	printf("Size of a long long int :%ld byte(s)\n" longLongType);
+	printf("Size of a float :%ld byte(s)\n", floatType);
 
 	return (0);
 }
