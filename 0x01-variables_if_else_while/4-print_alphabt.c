dad2d4000f7c2c50024ec6c3dc7be78cd4636085
Joel <joelinyang2003@gmail.com>
2022-07-12 02:49:10 -0700

    Joel

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,43 +1,24 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 #include <stdlib.h>
 
 
 
 /**
- *
- *  * main - print all letters of the alphabeth in lower case
- *
- *   *
- *
- *    * Return: 0 if successful
- *
- *     */
+ * main - print all letters of the alphabeth in lower case
+ * Return: 0 if successful
+ */
 
 int main(void)
-
 {
+	char var;
 
-		char var;
-
-
-
-			for (var = 'a'; var <= 'z'; var++)
-
-					{
-
-								if (var == 'e' || var == 'q')
-
-												continue;
-
-
-
-										putchar(var);
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	for (var = 'a'; var <= 'z'; var++)
+	{
+		if (var == 'e' || var == 'q')
+			continue;
+		putchar(var);
+	}
+	putchar('\n');
+	return (0);
 }
