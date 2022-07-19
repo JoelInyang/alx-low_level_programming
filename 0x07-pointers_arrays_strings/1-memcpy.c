993f43f751d0b9ee4049cb0e463687d45eb7920d
Joel <joelinyang2003@gmail.com>
2022-07-19 04:03:32 -0700

    Dil

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - function copies n byte from src to dest
+ * _memcpy - function
  * @dest: the address of memory to print
  * @src: the size of the memory to print
  * @n: the size of memory
  * Return: Nothing.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int a;
