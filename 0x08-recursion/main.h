75330479d9b7034cb1624bace142de37792cdd1e
Joel <joelinyang2003@gmail.com>
2022-07-24 14:52:55 -0700

    g

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,6 @@ int _sqrt_recursion(int n);
 int _sqrt(int a, int b);
 int is_prime_number(int n);
 int _is_prime(int a, int b);
+int is_palindrome(char *s);
+int _strlen(char *s);
+int _is_palindrome(char *s, int r, int l);
