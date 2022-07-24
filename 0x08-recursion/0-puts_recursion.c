b745d999dc2ddaa63a91d6eb3369bbd779739384
Joel <joelinyang2003@gmail.com>
2022-07-24 01:09:19 -0700

    f

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _puts_recursion - to recurse
+ *  @s: string
+ *  Return: Always 0.
+ */
+
+void _puts_recursion(char *s);
+{
+	puts("Joel is a great software engineer.\n");
+	return (0);
+}
