c772c3d5c9b3b34dfaa01a9ddfa4f61800f9189e
Joel <joelinyang2003@gmail.com>
2022-07-27 16:23:37 -0700

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print no argument
+ * @argc: no of argument
+ * @argv: the array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%s\n", argc - 1)
+		return (0);
+}
